dc73e0abdbef118654bf679941753da18ca3d06e
Nicolas Miguel Recto <recto.nicolas1106@gmail.com>
2021-03-10 16:00:17 +0800

    re commit crate add course

--- assets/js/addCourse.js
+++ assets/js/addCourse.js
@@ -1,3 +1,14 @@
+let authIsAdmin = localStorage['isAdmin'];
+
+let auth = () => {
+    if (authIsAdmin === 'false') {
+        alert('You are not allowed here!')
+        window.location.replace('./login.html')
+
+    }
+}
+auth();
+
 let createCourse = document.querySelector('#createCourse');
 
 createCourse.addEventListener('submit', (e) => {
@@ -8,9 +19,9 @@ createCourse.addEventListener('submit', (e) => {
     let description = document.querySelector('#description').value;
 
     let body = {
-        name: name,
-        price: price,
-        description: description
+        name,
+        price,
+        description
     }
 
     fetch('http://localhost:4000/api/courses/', {
@@ -24,7 +35,6 @@ createCourse.addEventListener('submit', (e) => {
         .then(res => res.json())
         .then(data => {
             if (!data) return ('Something went wrong');
-
 
             alert('Course created succesfully!')
 
