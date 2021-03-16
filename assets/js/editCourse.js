59b427c361553a0e3619232ff14ea92d691f77c3
Nicolas Miguel Recto <recto.nicolas1106@gmail.com>
2021-03-16 12:43:36 +0800

    add edit course feature

---
+++ assets/js/editCourse.js
@@ -0,0 +1,65 @@
+const urlParams = new URLSearchParams(window.location.search);
+const courseId = urlParams.get('courseId');
+
+let url = `http://localhost:4000/api/courses/${courseId}`;
+
+fetch(url)
+.then( res => res.json())
+.then(data => {
+// console.log(data)
+let editCourse = document.querySelector('#editCourse');
+
+return editCourse.innerHTML =
+`
+
+<div class="form-row">
+    <div class="form-group my-3">
+        <input type="text" name="" id="courseName" class="form-control" placeholder="Course Name" value="${data.name}">
+    </div>
+    <div class="form-group my-3">
+        <input type="number" name="" id="coursePrice" class="form-control" placeholder="Course Price" value="${data.price}">
+    </div>
+</div>
+<textarea name="" id="courseDescription" cols="30" rows="5" class="form-control my-3" placeholder="Course Description" value="">${data.description}</textarea>
+<button id="submitButton" class="btn btn-outline-primary">Submit</button>
+
+`
+})
+.then( result =>{
+ 	let submitButton = document.querySelector('#submitButton')
+	submitButton.addEventListener('click' , (e) => {
+		e.preventDefault();
+		let courseDescription = document.querySelector('#courseDescription').value;
+		let courseName = document.querySelector('#courseName').value;
+		let coursePrice = document.querySelector('#coursePrice').value;
+
+		let body = {
+			_id: courseId,
+			name: courseName,
+			price: coursePrice,
+			description: courseDescription
+		}
+
+		fetch('http://localhost:4000/api/courses/',
+			{
+	  			method: 'PUT',
+	  			headers: { 'Content-Type': 'application/json',
+	  						"Authorization": `Bearer ${localStorage['token']}`
+	  					 },
+	  			body: JSON.stringify(body)
+			})
+		.then( res => res.json())
+		.then( data => {
+
+			if(!data) return alert("Something went Wrong.");
+			
+			alert("Course Change.");
+			window.location.replace('./courses.html');
+
+		})
+
+	}); // end ofcallback function on submit
+
+})
+
+
