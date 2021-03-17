15282632dcc5406e4f64a9ea5d50eed5526bef32
Nicolas Miguel Recto <recto.nicolas1106@gmail.com>
2021-03-17 08:47:20 +0800

    add enroll course function

--- assets/js/course.js
+++ assets/js/course.js
@@ -7,7 +7,7 @@ let url = `http://localhost:4000/api/courses/${courseId}`;
 fetch(url)
 .then( res => res.json())
 .then(data => {
-// console.log(data)
+console.log(data)
 let courseName = document.querySelector('#courseName');
 let courseDesc = document.querySelector('#courseDesc');
 let coursePrice = document.querySelector('#coursePrice');
@@ -16,4 +16,31 @@ let coursePrice = document.querySelector('#coursePrice');
 courseName.innerHTML = data.name;
 courseDesc.innerHTML = data.description;
 coursePrice.innerHTML = data.price;
+
+document.querySelector('#enrollButton').addEventListener('click', () => {
+    
+    fetch('http://localhost:4000/api/users/enroll',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+                'Authorization' : `Bearer ${localStorage['token']}`
+            },
+            body: JSON.stringify({courseId})
+        }
+    )
+    .then( res => res.json() )
+    .then( data => {
+        if (!data) {
+            alert("Something Went Wrong")
+        } else {
+            alert("Thank You for Enroll. Please Check your Email for more information.")
+            window.location.replace('./courses.html')
+        }
+    })
+    
+})
+
+
+
 })
